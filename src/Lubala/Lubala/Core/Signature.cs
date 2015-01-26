c623b5d9fd6948d4c6607ed89eccc52f368fc007
Lu Rongkai <lurongkai@gmail.com>
2015-01-26 23:43:35 +0800

    add signature.

---
+++ src/Lubala/Lubala/Core/Signature.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lubala.Core
+{
+    internal class Signature
+    {
+        private string _signature;
+        public Signature(string signature)
+        {
+            _signature = signature;
+        }
+
+        public bool Validate(IRequestMessage request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
