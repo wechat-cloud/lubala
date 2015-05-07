81d17af48a886034e0c3753f72b8f287beae42cf
Lu Rongkai <lurongkai@gmail.com>
2015-05-07 21:46:11 +0800

    new changes.

--- src/Lubala/Cryptographic/EncodingAesKey.cs
+++ src/Lubala/Cryptographic/EncodingAesKey.cs
@@ -4,8 +4,20 @@ namespace Lubala
 {
 	public class EncodingAesKey
 	{
-		public EncodingAesKey()
+        private string _key;
+		public EncodingAesKey(string key)
 		{
+		    if (string.IsNullOrEmpty(key))
+		    {
+                throw new ArgumentException("key");
+		    }
+
+		    if (key.Length > 43)
+		    {
+                throw new ArgumentException("key should less than 43 characters.");
+		    }
+
+		    _key = key;
 		}
 	}
 }
